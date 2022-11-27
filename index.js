b5868ef91c8737cc429c9a6b56a2dc5f3157855f
dennis mwaniki <dennomaz@gmail.com>
2022-11-27 20:05:53 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const myArray = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(array, event){
+    let newArray = []
+
+    for(let i = 0; i < array.length; i++){
+        let message = `Thank you, ${myArray[i]}, for the wonderful ${event} gift!`
+        newArray.push(message)
+    }
+
+    return newArray
+}
+
+function countDown(){
+    let n = 10
+    while(n >= 0){
+        console.log(n)
+        n--
+    }
+}
